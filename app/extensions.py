b96cc442d323a79f9ab8dc6c13b24979e8a3476f
Emmanuel Ademola <mailemmydee@gmail.com>
2025-06-05 00:16:31 +0000

    feat: Add environment-based configuration for SocketIO initialization

--- app/extensions.py
+++ app/extensions.py
@@ -4,6 +4,8 @@ Initialize and provide instances of various Flask extensions.
 These extensions are configured and bound to the Flask application
 within the create_app factory function.
 """
+import os
+
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
@@ -14,9 +16,20 @@ from flask_sqlalchemy import SQLAlchemy
 # Extensions for the Flask application typed and initialized here
 db: SQLAlchemy = SQLAlchemy()
 jwt: JWTManager = JWTManager()
-socketio: SocketIO = SocketIO(
-    cors_allowed_origins="*",  # Allow CORS for WebSocket
-)
 api: Api = Api()
 cors: CORS = CORS()
 migrate: Migrate = Migrate()
+
+env = os.getenv("FLASK_ENV", "production")
+
+if env == "production":
+    socketio: SocketIO = SocketIO(
+        cors_allowed_origins="*",  # Allow CORS for WebSocket
+        async_mode="eventlet",  # Use eventlet for async support
+    )
+else:
+    socketio: SocketIO = SocketIO(
+        cors_allowed_origins="*",  # Allow CORS for WebSocket
+        async_mode="threading",  # Use threading for async support
+        #  in non-production
+    )
